d08a7e76103e4b9ac1d75056ebd1eac5c33c481c
Alex <therebeloper@gmail.com>
2018-10-01 20:05:21 +0300

    first commit

--- CosmosRatingTutorial/AppDelegate.swift
+++ CosmosRatingTutorial/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        let vc = ViewController()
+        window?.rootViewController = vc
+        
         return true
     }
 
--- CosmosRatingTutorial/ViewController.swift
+++ CosmosRatingTutorial/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import Cosmos
+import TinyConstraints
 
 class ViewController: UIViewController {
+    
+    lazy var cosmosView: CosmosView = {
+        var view = CosmosView()
+//        view.settings.updateOnTouch = false
+        view.settings.filledImage = UIImage(named: "RatingStarFilled")?.withRenderingMode(.alwaysOriginal)
+        view.settings.emptyImage = UIImage(named: "RatingStarEmpty")?.withRenderingMode(.alwaysOriginal)
+        
+        view.settings.totalStars = 10
+        view.settings.starSize = 17
+        view.settings.starMargin = 3.3
+        view.settings.fillMode = .precise
+        
+        view.text = "Rate me"
+        view.settings.textColor = .red
+        view.settings.textMargin = 10
+        
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
+        
+        view.addSubview(cosmosView)
+        cosmosView.centerInSuperview()
+        
+        cosmosView.didTouchCosmos = { rating in
+            print("Rated: \(rating)")
+        }
     }
 
 
